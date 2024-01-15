83910f521833b4ddfe9f32918661261773ec8afc
Julius M <143593015+TohveliDev@users.noreply.github.com>
2024-01-15 22:04:25 +0200

    Optimization

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,11 +7,8 @@ var player_state
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
-	if direction.x == 0 and direction.y == 0:
-		player_state = "idle"
-	elif direction.x != 0 or direction.y != 0:
-		player_state = "moving"
-		
+	player_state = "idle" if (direction == Vector2.ZERO) else "moving"
+
 	velocity = direction * speed
 	move_and_slide()
 
